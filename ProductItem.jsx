984bd447ef8e3ff36a71695794b879b1bd4a17d9
naxifa <maliha1@usf.edu>
2024-11-17 11:17:14 -0500

    Create ProductItem component to display individual product details

---
+++ ProductItem.jsx
@@ -0,0 +1,15 @@
+// Creating the ProductItem component
+
+import React from "react";
+
+function ProductItem({ product }) {
+  return (
+    <div>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
+}
+
+export default ProductItem;
